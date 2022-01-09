aafb504d5ee44f24514f7ea8830f53ef58883f7d
Mohamed Shahul Hameed <thameemsh@email.com>
2022-01-09 17:50:55 -0500

    Exercise 6: Create new employees in employees table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: 'Tom', last_name: 'Riddle', hourly_rate: 30)
+@store1.employees.create(first_name: 'Charlie', last_name: 'Brown', hourly_rate: 50)
+@store1.employees.create(first_name: 'Green', last_name: 'Lantern', hourly_rate: 15)
+
+@store2.employees.create(first_name: 'Jay', last_name: 'Who', hourly_rate: 100)
+@store2.employees.create(first_name: 'John', last_name: 'Smith', hourly_rate: 50)
+@store2.employees.create(first_name: 'Jane', last_name: 'Doe', hourly_rate: 30)
